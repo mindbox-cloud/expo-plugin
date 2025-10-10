ae6aca8f54f80e25a102859a9b6e9ba034cbe89a
sozinov <sozinov@mindbox.cloud>
2025-10-10 09:22:20 +0300

    WMSDK-541: follow review

--- src/withMindbox.ts
+++ src/withMindbox.ts
@@ -3,10 +3,8 @@ import { withMindboxAndroid } from "./withMindboxAndroid";
 import { withMindboxIos } from "./withMindboxIos";
 import { validatePluginProps } from "./utils/validation";
 import type { MindboxPluginProps } from "./mindboxTypes";
-import { logSuccess } from "./android/utils/errorUtils";
 
 const withMindbox: ConfigPlugin<MindboxPluginProps> = (config, props = {}) => {
-  logSuccess("withMindbox initialization");
   validatePluginProps(props);
   config = withMindboxAndroid(config, props);
   config = withMindboxIos(config, props);

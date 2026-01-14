740c10a3c7a97b3ea99bd1d01dafa3b7785ec4d2
sozinov <sozinov@mindbox.cloud>
2026-01-14 16:37:39 +0300

    WMSDK-567: Auto-generate EAS credentials configuration for NSE and NCE extensions

--- src/withMindbox.ts
+++ src/withMindbox.ts
@@ -2,10 +2,17 @@ import { ConfigPlugin, withAppBuildGradle } from "@expo/config-plugins";
 import { withMindboxAndroid } from "./withMindboxAndroid";
 import { withMindboxIos } from "./withMindboxIos";
 import { validatePluginProps } from "./utils/validation";
+import { configureMindboxEasCredentials } from "./helpers/easCredentialsConfig";
 import type { MindboxPluginProps } from "./mindboxTypes";
 
 const withMindbox: ConfigPlugin<MindboxPluginProps> = (config, props = {}) => {
   validatePluginProps(props);
+
+  config = {
+    ...config,
+    extra: configureMindboxEasCredentials(config)
+  };
+  
   config = withMindboxAndroid(config, props);
   config = withMindboxIos(config, props);
   return config;

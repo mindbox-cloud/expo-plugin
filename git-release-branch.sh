edbae1782be3208e7a8c63adef48667458f24877
sozinov <sozinov@mindbox.cloud>
2026-06-15 14:02:22 +0300

    MOBILE-121: bump GitHub action versions

--- git-release-branch.sh
+++ git-release-branch.sh
@@ -1,35 +1,36 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
 current_branch=$(git symbolic-ref --short HEAD)
 
-if [[ $current_branch != "develop" && ! $current_branch =~ ^release/[0-9]+\.[0-9]+\.[0-9]+(-rc)?$ ]]; then
+if [[ "$current_branch" != "develop" && ! "$current_branch" =~ ^release/[0-9]+\.[0-9]+\.[0-9]+(-rc)?$ ]]; then
   echo "The current Git branch ($current_branch) is not 'develop' or in the format 'release/X.Y.Z' or 'release/X.Y.Z-rc'."
   exit 1
 fi
 
 # Check if the parameter is provided
-read -p "Mindbox Expo plugin version: " version
+read -r -p "Mindbox Expo plugin version: " version
 
 # Check if the version number matches the semver format
-if ! [[ $version =~ ^[0-9]+\.[0-9]+\.[0-9]+(-rc)?$ ]]; then
+if ! [[ "$version" =~ ^[0-9]+\.[0-9]+\.[0-9]+(-rc)?$ ]]; then
   echo "The release version number does not match the semver format (X.Y.Z or X.Y.Z-rc)."
   exit 1
 fi
 
 
 branch_name="release/$version"
-git branch $branch_name
-git checkout $branch_name
+git branch "$branch_name"
+git checkout "$branch_name"
 
 echo "Branch $branch_name has been created."
 
 package_json="package.json"
-current_version=$(grep -Eo '"target-version": "[^"]+"' $package_json | cut -d '"' -f 4)
-sed -i '' "s/\"target-version\": \".*\"/\"target-version\": \"$version\"/" $package_json
+current_version=$(grep -Eo '"target-version": "[^"]+"' "$package_json" | cut -d '"' -f 4)
+sed -i '' "s/\"target-version\": \".*\"/\"target-version\": \"$version\"/" "$package_json"
 
 echo "Bump SDK version from $current_version to $version."
 
-git add $package_json
+git add "$package_json"
 
 changelog="CHANGELOG.md"
 
